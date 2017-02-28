957c769afedd838ad4a438801173c502b52c31f8
WeichenXu <weichenxu1993@icloud.com>
2017-02-27 22:47:52 -0500

    LC 277 more explicit

--- findTheCelebrity.cpp
+++ findTheCelebrity.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-// 1. Maintain a possible celebrity list
+// 1. locate the only possible celebrity 
+// 2. whether it is valid
 
 using namespace std;
 
@@ -10,52 +11,18 @@ using namespace std;
 class Solution {
 public:
     int findCelebrity(int n) {
-        vector<bool> people(n, true);
-        int cel;
-        // 1. find a possible candidate, check who he knows
-        while ( (cel = findCandidate(people)) != -1 ){
-            // if everyone know him
-            bool allKnown = true;
-            for (int i=0; i<n; ++i){
-                if (i == cel)   continue;
-                if (!knows(i, cel)){
-                    allKnown = false;
-                    break;
-                }
-                people[i] = false;
+        int cel = 0;
+        if (n <= cel)   return -1;
+        for (int i=0; i<n; ++i){
+            if (!knows(i, cel)){
+                cel = i;
             }
-            // if he doesnt know everyone
-            if (allKnown){
-                bool knowNone = true;
-                for (int i=0; i<n; ++i){
-                    if (i == cel)   continue;
-                    if (knows(cel, i)){
-                        knowNone = false;
-                        break;
-                    }
-                    people[i] = false;
-                }
-                if (knowNone){
-                    return cel;
-                }
-            }
-            people[cel] = false;
         }
-
-        return -1;
-    }
-private:
-    int findCandidate(vector<bool>& people){
-        for (int i=0; i<people.size(); ++i){
-            if (people[i])  return i;
+        for (int i=0; i<n; ++i){
+            if (i == cel)   continue;
+            if (knows(cel, i) || !knows(i, cel))    return -1;
         }
-        return -1;
-    }
-    bool knows(int a, int b){
-        if (b == 1 || a== b){
-            return true;
-        }
-        return false;
+        return cel;
     }
 };
 

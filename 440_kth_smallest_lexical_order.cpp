0dc9cd9f40776f87b7071d8f6befd6b6442ef837
WeichenXu <weichenxu1993@icloud.com>
2017-04-22 15:45:53 -0400

    LC 440 using denery tree

--- 440_kth_smallest_lexical_order.cpp
+++ 440_kth_smallest_lexical_order.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
+#include <algorithm>
+/*
+0 | 1 | 2 | ........        Level cur,
+|   \
+|     \
+|       \
+0,1,2...9|0,1,2,3 ....      Level cur+1
+*/
 
-// 1. Iterate all k numbers in the range of n
-//    a. append '0' as much as possible
-//    b. ++ num if smaller than n
+// 2. Denery tree
+//    a.  Main function: 
+//          If steps remaing is larger, go to next in Level cur
+//          Else go to Level cur+1
+//    b.  Calculate steps:
+//          Return the steps between two node in Same Level
 using namespace std;
 
 class Solution {
+private:
+    // return the steps between n1&n2 in range[1,n] based on Denery tree
+    int calSteps(int n, long n1, long n2){
+        long steps = 0;
+        while (n1  <= n){
+            steps += min((long)n+1, n2) - n1;
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
+    }
+
 public:
     int findKthNumber(int n, int k) {
-        if (k <= 0 || k > n){
-            return 0;
-        }
-        int i = 1, cur = 1;
-        while (i < k){
-            int nx;
-            if (cur*10 <= n){
-                nx = cur * 10;
+        if (k > n || k <= 0) return 0;
+        long cur = 1;
+        --k;
+        while(k > 0){
+            int steps = calSteps(n, cur, cur+1);
+            if (steps <= k){
+                k -= steps;
+                ++cur;
             }
-            else if (cur+1 <= n){
-                nx = cur+1;
-                while (! (nx % 10)){
-                    nx /= 10;
-                }
+            else{
+                cur *= 10;
+                --k;
             }
-            else {
-                nx = cur/10 + 1;
-            }
-            cur = nx;
-            ++i;
         }
         return cur;
     }
 };
 
 int main(){
-    int n = 719885387, k=209989719;
+    int n = 10, k=3;
     Solution sol;
     auto res = sol.findKthNumber(n, k);
     cout << "Res: " << res << endl;

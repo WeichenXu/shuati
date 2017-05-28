c45f756f8846fb2c50af2b40dcfe2d177a7a13cb
WeichenXu <weichenxu1993@icloud.com>
2017-05-28 08:47:25 +0800

    LC 418 with memorization&DP

--- 418_sentence_screen.cpp
+++ 418_sentence_screen.cpp
@@ -2,47 +2,43 @@
 #include <string>
 #include <vector>
 
-// 1. Formatted strings match, directly 
+// 1st solution. Formatted strings match, directly 
+// 2nd solution, DP: generate array which store next index
+//                 ** next index might be longer than len of array
 
 using namespace std;
 
 class Solution {
 public:
     int wordsTyping(vector<string>& sentence, int rows, int cols) {
-        string pattern = "";
-        for (const auto& word:sentence){
-            pattern += word + " ";
-        }
-        cout << "Pattern: " << pattern << endl;
-        int len = pattern.length(), start = 0;
-        // match the sentence by each row
-        for (int i=0; i<rows; ++i){
-            cout << "Current start: " << start << endl;
-            start += cols;
-            if (pattern[start%len] == ' '){
-                ++start;
+        int size = sentence.size();
+        vector<int> nx(size, 0);
+        for (int i=0; i<size; ++i){
+            int cur = i, len = 0;
+            while (len + sentence[cur % size].length() <= cols){
+                len += sentence[cur++ % size].length() + 1;
             }
-            else{
-                while(start > 0 && pattern[(start-1)%len] != ' '){
-                    cout << pattern[(start-1)%len] << endl;
-                    --start;
-                }
-            }
+            cout << "Current index: " << i << ", current nx: " << cur << endl;
+            nx[i] = cur;
         }
 
-        cout << "Current start: " << start << endl;
-        return start/len;
+        int k = 0;
+        for (int i=0; i<rows; ++i){
+            k += nx[k%size] - k%size;
+        }
+        return k/size;
     }
 };
 
 int main(){
     vector<string> sentence = {
-        "a",
-        "bcd",
-        "f"
+        "I",
+        "had",
+        "apple",
+        "pie"
     };
     Solution sol;
-    int rows = 2, cols = 5;
+    int rows = 4, cols = 5;
     auto res = sol.wordsTyping(sentence, rows, cols);
     cout << "Could match times: " << res << endl;
     return 0;

2e511fe2b56c2e3363bc2cf6a4dce894736af841
WeichenXu <weichenxu1993@icloud.com>
2017-08-20 14:07:37 -0400

    crtp static interface

--- crtpII.cpp
+++ crtpII.cpp
@@ -25,12 +25,50 @@ private:
     double m_value{0.0};
 };
 
+template <typename T>
+class Amount{
+public:
+    double getValue() const{
+        return static_cast<T const&>(*this).getValue();
+    }
+};
+
+class ConstAmount : public Amount<ConstAmount>{
+public:
+    double getValue() const{
+        return 42.0;
+    }
+};
+
+class Variable : public Amount<Variable>{
+public:
+    double getValue() const{
+        return m_value;
+    }
+    explicit Variable(double v) : m_value(v){}
+private:
+    double m_value;
+};
+
+// print interface for amount
+template<typename T>
+void print(Amount<T> const& amount){
+    std::cout << amount.getValue() << std::endl;
+}
 
 int main(){
+    // extend functions
     Sensitivity s;
     s.setValue(1.0);
     s.scale(100);
     std::cout << s.getValue() << std::endl;
+
+    // static interface
+    ConstAmount c;
+    print(c);
+    Variable v(43.00);
+    print(v);
+
     return 0;
 }
 
